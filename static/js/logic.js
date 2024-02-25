3b5473951294ad4f6427978ffaf0b820b5556fb3
Qazi Fabia Hoq <52504271+qazifabiahoq@users.noreply.github.com>
2024-02-24 23:20:29 -0500

    commit

---
+++ static/js/logic.js
@@ -0,0 +1,88 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL.
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+
+  // Function to determine the radius of the circle marker based on the earthquake magnitude
+  function radiusSize(magnitude) {
+    return magnitude * 5;
+  }
+
+  // Function to determine the color of the circle marker based on the earthquake depth
+  function depthColor(depth) {
+    if (depth > 90) {
+      return "red";
+    } else if (depth > 70) {
+      return "orange";
+    } else if (depth > 50) {
+      return "yellow";
+    } else if (depth > 30) {
+      return "green";
+    } else if (depth > 10) {
+      return "blue";
+    } else {
+      return "purple";
+    }
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: radiusSize(feature.properties.mag),
+        fillColor: depthColor(feature.geometry.coordinates[2]),
+        color: "black",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    },
+    onEachFeature: onEachFeature
+  });
+
+  // Send our earthquakes layer to the createMap function.
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
